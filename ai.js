d5df89d8e427eb0ca74d1b994c6428a7c5d7ab1c
Jonas Almeida <jalmeida@mathbiol.org>
2019-06-19 22:19:58 -0400

    keeping up with Marie's tfvis exploits

--- ai.js
+++ ai.js
@@ -1,7 +1,7 @@
 console.log('ai.js loaded');
 
 
-(function(){
+(async function(){
 
 const ai={}
 
@@ -10,15 +10,58 @@ ai.created_at=Date.now()
 ai.getIris = async function(){
     return (await fetch('https://episphere.github.io/ai/data/iris.json')).json()
 }
+ai.getCars = async function(){
+    return (await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')).json()
+}
 
+ai.getScript=async function(url){
+    url=url||'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js'
+    return new Promise(function(resolve, reject) {
+        let s = document.createElement('script')
+        s.src=url
+        s.onload=resolve
+        s.onerror=reject
+        document.head.appendChild(s)
+    })
+}
+
+ai.codeLab=async function(){ // https://codelabs.developers.google.com/codelabs/tfjs-training-regression
+    let cars = await ai.getCars()
+    let values = cars.map(c=>{return{
+        //debugger
+        x:c.Horsepower,
+        y:c.Miles_per_Gallon
+    }})
+    tfvis.render.scatterplot(
+        {name: 'Horsepower v MPG'},
+        {values}, 
+        {
+          xLabel: 'Horsepower',
+          yLabel: 'MPG',
+          height: 300
+        }
+      )
+}
+
+
+
+
+
+if(typeof(window)=='object'){ // regular web browser application
+    window.onload=async function(){
+        await ai.getScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js')
+        ai.tf=tf
+        await ai.getScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js')
+        ai.tfvis=tfvis
+        window.ai=ai
+        // check if intro is in order
+        console.log('window loaded')
+        ai.codeLab()
+        //define(ai)
+    }
+}
 if(typeof(define)!=='undefined'){ // loaded as a required object
     define(ai)
-}else if(typeof(window)=='object'){ // regular web browser application
-    window.ai=ai
-    // check if intro is in order
-    window.onload=function(){
-        console.log('window loaded')
-    }
 }
 
 
